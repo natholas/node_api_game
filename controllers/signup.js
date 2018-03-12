5bd6581c885f13eda73a3df667b79295f56f3c11
Nathan Felix <natholas@gmail.com>
2018-03-12 19:43:16 +0100

    Added celestial assigning to signup

--- controllers/signup.js
+++ controllers/signup.js
@@ -10,9 +10,22 @@ module.exports = function (req, res) {
   })
   .on('end', function() {
     if (rows.length) return res({ error: 'USER_EXISTS' })
+    var playerId
     sql.query('INSERT INTO player (username, password_hash) VALUES (?,?)', [req.body.username, hash])
-    .on('end', function (rows) {
-      return res({ error: false })
+    .on('result', (row) => playerId = row.insertId)
+    .on('end', function () {
+      sql.query('UPDATE celestial SET owner_id = ? WHERE owner_id IS NULL AND type = "planet" ORDER BY RAND() LIMIT 1', [playerId*1])
+      .on('end', function() {
+        var planetId
+        sql.query('SELECT * FROM celestial WHERE owner_id = ?', [playerId])
+        .on('result', (row) => planetId = row.insertId)
+        .on('end', () => {
+          sql.query('INSERT into celestial_facilities (planet_id) VALUES (?)', [planetId])
+          .on('end', () => {
+            return res({ error: false })
+          })
+        })
+      })
     })
   })
 }
