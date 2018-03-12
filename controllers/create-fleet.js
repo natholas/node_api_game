0abaca568395003e0242a147f0920ff28087e74b
Nathan Felix <natholas@gmail.com>
2018-03-12 18:39:28 +0100

    Fix for create fleet

--- controllers/create-fleet.js
+++ controllers/create-fleet.js
@@ -2,21 +2,23 @@ const sql = require('../connection')
 
 module.exports = function (req, res) {
   var rows = []
-  var fleetId = null;
-  sql.query('INSERT INTO fleet (pos_x, pos_y, owner_id) VALUES (?,?,?)', [req.body.pos.x, req.body.pos.y, req.user.id])
-  .on('result', function(row) {
-    fleetId = row.insertId
+  sql.query('SELECT * FROM ship WHERE ship_id IN (?) AND pos_x = ? AND pos_y = ? AND owner_id = ? AND fleet_id IS NULL', [req.body.shipIds, req.body.pos.x, req.body.pos.y, req.user.id])
+  .on('result', (row) => {
+    rows.push(row)
   })
-  .on('end', function() {
-    var response
-    sql.query('UPDATE ship SET fleet_id = ?, planet_id = null WHERE ship_id IN (?) AND pos_x = ? AND pos_y = ? AND owner_id = ? AND fleet_id IS NULL',
-    [fleetId, req.body.shipIds, req.body.pos.x, req.body.pos.y, req.user.id])
+  .on('end', () => {
+    if (!rows.length) return res({ error: 'NO_SHIPS_FOUND' })
+    var fleetId = null;
+    sql.query('INSERT INTO fleet (pos_x, pos_y, owner_id) VALUES (?,?,?)', [req.body.pos.x, req.body.pos.y, req.user.id])
     .on('result', function(row) {
-      response = row
+      fleetId = row.insertId
     })
     .on('end', function() {
-      if (!response || response.affectedRows == 0) return res({ error: 'NO_SHIPS_FOUND' })
-      return res({ error: false })
+      sql.query('UPDATE ship SET fleet_id = ?, planet_id = null WHERE ship_id IN (?) AND pos_x = ? AND pos_y = ? AND owner_id = ? AND fleet_id IS NULL',
+      [fleetId, req.body.shipIds, req.body.pos.x, req.body.pos.y, req.user.id])
+      .on('end', function() {
+        return res({ error: false })
+      })
     })
   })
 }
